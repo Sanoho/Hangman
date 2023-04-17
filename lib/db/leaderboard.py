070f011d9041ac8e770ca2084df3106bdea21a92
Timothy <sanohoang@gmail.com>
2023-04-17 16:24:14 -0500

    updated classes

--- lib/db/leaderboard.py
+++ lib/db/leaderboard.py
@@ -1,4 +1,21 @@
-class Leaderboard():
-    def __init__(self, user, score) -> None:
-        self.user = user
+from sqlalchemy import (Column, String, Integer)
+from sqlalchemy.ext.declarative import (declarative_base)
+
+Base = declarative_base()
+
+class Leaderboard(Base):     
+    __tablename__ = 'leaderboard'
+
+    id = Column('id', Integer, primary_key = True)
+    username = Column('username', String)
+    score = Column('score', Integer)
+
+    def __init__(self, username, score) -> None:
+        self.username = username
         self.score = score
+
+    def __repr__(self):
+        return f'Id: {self.id}, ' \
+            f'Username {self.username}, ' \
+            f'Score {self.score}, ' \
+            
--- lib/db/user.py
+++ lib/db/user.py
@@ -1,8 +1,18 @@
-from sqlalchemy import (PrimaryKeyConstraint, Column, String, Integer)
+from sqlalchemy import (Column, String, Integer)
 from sqlalchemy.ext.declarative import (declarative_base)
 
 Base = declarative_base()
 
 class User(Base):
+    __tablename__ = 'users'
+
+    id = Column('id', Integer, primary_key = True)
+    username = Column('username', String)
+
     def __init__(self, username) -> None:
         self.username = username
+
+    def __repr__(self):
+        return f'Id: {self.id}, ' \
+            f'Username {self.username}, ' \
+            
