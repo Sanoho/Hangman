22d2ed778518c41d504b81ec27632c689955a655
Timothy <sanohoang@gmail.com>
2023-04-17 15:49:49 -0500

    added classes

---
+++ lib/db/leaderboard.py
@@ -0,0 +1,4 @@
+class Leaderboard():
+    def __init__(self, user, score) -> None:
+        self.user = user
+        self.score = score
---
+++ lib/hangman.py
@@ -0,0 +1,3 @@
+from db.score import Score
+from db.user import User
+from db.words import Words
