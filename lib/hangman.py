dc6147a87c29db65c92a10dd31d997c3fd9340f3
Erkut Cakmak <cakmak.erkut1@gmail.com>
2023-04-18 19:54:08 +0300

    game functionality

--- lib/hangman.py
+++ lib/hangman.py
@@ -1,7 +1,32 @@
 # from db.score import Score
 # from db.user import User
-# from db.words import Words
+import level1, level2, level3, level4, level5
 import os, time, sys
+
+os.system('clear')
+hangman = ["hangman11.txt", "hangman22.txt"]
+
+# def animator(filenames, delay = 1, repeat = 4):
+#     frames = []
+#     for name in filenames:
+#         with open (name, 'r', encoding = 'utf8') as f:
+#             frames.append(f.readlines())
+#     for i in range(repeat):
+#         for frame in frames:
+#             print(''.join(frame))
+#             time.sleep(delay)
+#             os.system('clear')
+
+# animator(hangman, delay = 1.0, repeat = 3)
+
+# message = """Welcome to Hangman!
+# Hangman is a classic word game in which you must guess as many secret words as you can before you run out of lives!\n"""
+# def title_typewriter(message):
+#     for char in message:
+#         sys.stdout.write(char)
+#         sys.stdout.flush()
+#         time.sleep(0.1)
+# title_typewriter(message)
 
 ask_name = "Please enter your username:\n"
 def prompt_username(ask_name):
@@ -9,132 +34,30 @@ def prompt_username(ask_name):
         sys.stdout.write(char)
         sys.stdout.flush()
         time.sleep(0.1)
+prompt_username(ask_name)
 
-class Hangman():
-    os.system('clear')
-    hangman = ["hangman11.txt", "hangman22.txt"]
+username = input()
+def input_username(username):
+    welcome_message = f"Welcome, {username}!\nAre you ready to start?\n(y/n)\n"
+    for char in welcome_message:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.1)
+input_username(username) 
 
-    def animator(filenames, delay = 1, repeat = 4):
-        frames = []
-        for name in filenames:
-            with open (name, 'r', encoding = 'utf8') as f:
-                frames.append(f.readlines())
-        for i in range(repeat):
-            for frame in frames:
-                print(''.join(frame))
-                time.sleep(delay)
-                os.system('clear')
+decision = input()
 
-    animator(hangman, delay = 1.0, repeat = 3)
+def yes_or_no(decision):
+    if decision == "y":
+        print("Let's start the game!")
 
-    message = """Welcome to Hangman!
-Hangman is a classic word game in which you must guess as many secret words as you can before you run out of lives!\n"""
-    def title_typewriter(message):
-        for char in message:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            time.sleep(0.1)
-    title_typewriter(message)
-
+while decision != "y":
     prompt_username(ask_name)
-
     username = input()
-    def input_username(username):
-        welcome_message = f"Welcome, {username}!\nAre you ready to start?\n(y/n)\n"
-        for char in welcome_message:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            time.sleep(0.1)
-
-    input_username(username) 
+    input_username(username)
     decision = input()
-    def yes_or_no(decision):
-        if decision == "y":
-            print("Let's start the game!")
-        else:
-            prompt_username(ask_name)
     yes_or_no(decision)
 
+level5.main()
 
 
-# GET WORD FUNC
-
-# PLAY FUNC
-
-# DISPLAY STAGES OF HANGMAN FUNC
-
-# def display_hangman(tries):
-#     stages = [  # final state: head, torso, both arms, and both legs
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |     /|\
-#                    |      |
-#                    |     / \
-#                    -
-#                 """,
-#                 # head, torso, both arms, and one leg
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |     /|\
-#                    |      |
-#                    |     / 
-#                    -
-#                 """,
-#                 # head, torso, and both arms
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |     /|\
-#                    |      |
-#                    |      
-#                    -
-#                 """,
-#                 # head, torso, and one arm
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |     /|
-#                    |      |
-#                    |     
-#                    -
-#                 """,
-#                 # head and torso
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |      |
-#                    |      |
-#                    |     
-#                    -
-#                 """,
-#                 # head
-#                 """
-#                    --------
-#                    |      |
-#                    |      O
-#                    |    
-#                    |      
-#                    |     
-#                    -
-#                 """,
-#                 # initial empty state
-#                 """
-#                    --------
-#                    |      |
-#                    |      
-#                    |    
-#                    |      
-#                    |     
-#                    -
-#                 """
-#     ]
-#     return stages[tries]
-
-# MAIN FUNC
